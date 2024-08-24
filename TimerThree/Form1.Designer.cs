6754a41a413ee35912052ae1629d93632bd7be60
zenin5479 <zenin5479@gmail.com>
2024-08-24 09:52:48 +0300

    Update 1.2 Обновлен метод

--- TimerThree/Form1.Designer.cs
+++ TimerThree/Form1.Designer.cs
@@ -28,10 +28,18 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.components = new System.ComponentModel.Container();
-         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(800, 450);
-         this.Text = "Form1";
+         SuspendLayout();
+         // 
+         // Form1
+         // 
+         AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+         ClientSize = new System.Drawing.Size(483, 396);
+         Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+         MaximizeBox = false;
+         MinimizeBox = false;
+         Name = "Form1";
+         Text = "Таймер";
+         ResumeLayout(false);
       }
 
       #endregion
