4e6c5cf966d2fbc7531a80e1e07acbc2c9577033
zenin5479 <zenin5479@gmail.com>
2024-08-24 09:50:59 +0300

    Update 1.1 Обновлен метод

---
+++ TimerThree/Form1.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+// System.Threading.Timer
+
+namespace TimerThree
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+   }
+}
